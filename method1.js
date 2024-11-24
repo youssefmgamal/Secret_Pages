e5d75d2765d42374bc2cc1c1a929bc98eaa707c9
Youssefmgamal <youssef_mgamal@hotmail.com>
2024-11-24 19:50:02 +0200

    secretPages

---
+++ method1.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+function checkPassword(req, res, next) {
+    const password = req.body['password'];
+    if (password.toLowerCase() === 'iloveprogramming') {
+        next();
+    } else {
+        res.sendFile(__dirname + '/public/index.html');
+}}
+
+app.use(checkPassword);
+
+app.post('/check', (req, res) => {
+    res.sendFile(__dirname + '/public/secret.html');
+});
+    
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
